3bc93d4155980a51981ebf1a7bdecba16fc1fbec
flundermicha <michael@flunder.io>
2017-07-04 16:17:34 +0200

    Changed tags for formats `Bold` and `Italic`
    * `Bold` uses `<b>` tag now (instead of `<strong>`)
    * `Italic` uses `<i>` tag now (instead of `<em>`)

--- formats/bold.js
+++ formats/bold.js
@@ -1,22 +1,7 @@
 import Inline from '../blots/inline';
 
-class Bold extends Inline {
-  static create() {
-    return super.create();
-  }
-
-  static formats() {
-    return true;
-  }
-
-  optimize(context) {
-    super.optimize(context);
-    if (this.domNode.tagName !== this.statics.tagName[0]) {
-      this.replaceWith(this.statics.blotName);
-    }
-  }
-}
+class Bold extends Inline { }
 Bold.blotName = 'bold';
-Bold.tagName = ['STRONG', 'B'];
+Bold.tagName = 'B';
 
 export default Bold;
